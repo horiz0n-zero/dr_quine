2a999d442f697b789acc1f6ed6db042d4c17a65a
Antoine Feuerstein <antoinefeufeu@antoines-macbook-pro.home>
2018-11-27 12:53:40 +0100

    Grace.s done

--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,31 +1,46 @@
-%define DATA src db "hello"
-%define
-%define
+%define SOURCE "%%define SOURCE %4$c%1$s%4$c%2$c%%define FILE %4$cGrace_kid.s%4$c%2$c%%define NONONONO _main%2$c%2$csection .data%2$csrc db SOURCE, 0%2$cfile db FILE, 0%2$c%2$csection .text%2$cglobal _start%2$cglobal _main%2$cglobal _duplicate%2$cextern _dprintf%2$c%2$c_start:%2$ccall NONONONO%2$cret%2$c%2$cNONONONO:%2$ccall _duplicate%2$cret%2$c%2$c%3$c%2$c%3$c open(5) close(6)%2$c%3$c%2$c%2$c_duplicate:%2$cpush rbp%2$cmov rbp, rsp%2$csub rsp, 24%2$clea rdi, [rel file]%2$cmov rsi, 0x0202%2$cmov rdx, 644o%2$cmov rax, 0x2000005%2$csyscall%2$cmov rdi, rax%2$clea rsi, [rel src]%2$clea rdx, [rel src]%2$cmov rcx, 10%2$cmov r8, 59%2$cmov r9, 34%2$ccall _dprintf%2$cmov rax, 0x2000006%2$csyscall%2$cleave%2$cret%2$c"
+%define FILE "Grace_kid.s"
+%define NONONONO _main
 
 section .data
-	DATA
+src db SOURCE, 0
+file db FILE, 0
 
 section .text
-	global _start
-	global _main
-	global _duplicate
-	extern _dprintf
-	extern _open
+global _start
+global _main
+global _duplicate
+extern _dprintf
 
 _start:
-	call _main
-	ret
+call NONONONO
+ret
 
-_main:
-	call _duplicate
-	ret
+NONONONO:
+call _duplicate
+ret
+
+;
+; open(5) close(6)
+;
 
 _duplicate:
-	push rbp
-	mov rbp, rsp
-	sub rsp, 16
-	
-	ret
-;
-;	duplicate
-;
+push rbp
+mov rbp, rsp
+sub rsp, 24
+lea rdi, [rel file]
+mov rsi, 0x0202
+mov rdx, 644o
+mov rax, 0x2000005
+syscall
+mov rdi, rax
+lea rsi, [rel src]
+lea rdx, [rel src]
+mov rcx, 10
+mov r8, 59
+mov r9, 34
+call _dprintf
+mov rax, 0x2000006
+syscall
+leave
+ret
